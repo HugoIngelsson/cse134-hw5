1470127f1bd09ddf8525cf8e8844626dc3e4fdb8
HugoIngelsson <cingelsson@ucsd.edu>
2025-11-17 14:50:49 -0800

    Created view transition

--- media/scripts/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', init)
-
-function init() {
-    const sliders = document.querySelectorAll('sliding-container');
-    sliders.forEach((elem) => {
-        document.startViewTransition(() => updateTheDOMSomehow(elem));
-    })
-}
